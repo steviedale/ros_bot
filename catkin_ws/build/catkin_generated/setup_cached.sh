351fb67bfe90281f45590fe9983d536f23def345
Stevie Dale <sdale4@asu.edu>
2019-10-14 23:21:33 -0700

    playing around with folder structure and ros setup

---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/stevie/git/ros_bot/catkin_ws/devel:$CMAKE_PREFIX_PATH"
+export PWD="/home/stevie/git/ros_bot/catkin_ws/build"
+export ROSLISP_PACKAGE_DIRECTORIES="/home/stevie/git/ros_bot/catkin_ws/devel/share/common-lisp"
+export ROS_PACKAGE_PATH="/home/stevie/git/ros_bot/catkin_ws/src:$ROS_PACKAGE_PATH"
